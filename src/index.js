bc172b09386a81b69f7285f63738534de1ca3535
yashovardhan-228 <eryashovardhansingh@gmail.com>
2026-03-07 19:29:31 +0530

    Utils Folder making

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './DB/db.js'
+import { app } from './app.js'
 
 dotenv.config({path: './env'})
 
+
+const port= process.env.PORT || 4000
 connectDB()
+.then(()=>{
+    app.listen(port, ()=>{
+        console.log(`Server is running at port : ${port}`);
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB Connection Failed !!!", error);
+})
